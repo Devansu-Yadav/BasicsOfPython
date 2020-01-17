7596f60dba2ea2fa9b64a441adcb7eff9aac2fff
imabp <abir.pal899@gmail.com>
2020-01-17 20:14:51 +0530

    Added 1.py in Unit 2

--- Unit2/1.py
+++ Unit2/1.py
@@ -1,1 +1,11 @@
 # Program to demonstrate the various kind of operations that can be applied to the string.
+s="hello World"
+
+print(s.capitalize())
+#It converts first letter of the string to captial letter. Without affecting the original value of the stirng.
+
+print(s.center(24))
+# The center() method returns a string padded with specified fillchar. It doesn't modify the original string.
+
+print(s.casefold()) 
+#The casefold() method is removes all case distinctions present in a string. It is used for caseless match
