d68c48fa405d4b586c2a32759085eef43b328565
Devansu <devansuyadav@gmail.com>
2020-10-01 15:08:07 -0700

    Added file for Unit 1 Part 3 Q6

--- Unit1/Part3/6.py
+++ Unit1/Part3/6.py
@@ -1,1 +1,24 @@
-# Scope of variable.
+def scope_variable():
+    print(msg)
+    # This code shows error because we are referencing 'msg' before assignment inside the function.
+    # If we comment the line below no error will be shown.
+    msg = "This is local variable" 
+    print(msg)
+
+msg = "This is Global variable"
+scope_variable()
+print(msg)
+
+
+# Corrected Code:-
+# Uncomment this code to use this code and comment the above code.
+# def scope_variable():
+#     # If we comment the line below no error will be shown.
+#     global msg
+#     print(msg)
+#     msg = "This is local variable" 
+#     print(msg)
+
+# msg = "This is Global variable"
+# scope_variable()
+# print(msg)
